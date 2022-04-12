517221ba1d5eae9700a4af2ab1a8e7a1025b503e
Michael Staab <michael.staab@student.hswt.de>
2022-04-12 16:28:39 +0200

    register react-frontend app

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request, *args, **kwargs):
+    # Finds the template beacause of this:
+    # https://docs.djangoproject.com/en/4.0/topics/templates/#django.template.backends.django.DjangoTemplates
+    return render(request, 'frontend/index.html')
