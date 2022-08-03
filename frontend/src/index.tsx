0dcdda31491cb998fecca57e477af1ee54cb5316
Adrian Wild <adrian.wild@student.hswt.de>
2022-08-03 10:34:59 +0200

    Bug fixes
    - Change plot label format
    - Fix unique key error
    - Validate MeasureInputField input

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,19 +9,19 @@ import App from "./components/App";
 import store from "./store";
 import layoutConfig from "./configuration/LayoutConfig";
 import pageDefinitions, {pageConfig} from "./configuration/PageConfig";
-
+import { createRoot } from 'react-dom/client';
 // Entry point of the react-application. Requires a div with id equal to "app"
 const appDiv = document.getElementById("app");
-
+const root = createRoot(appDiv!);
 /**
  * Render the app-component inside the app-div and pass various configuration
  * parameters as well as the redux-store to be used by the react-application.
  */
-render(
+root.render(
     <App
         store={store}
         layoutConfig={layoutConfig}
         pageDefinitions={pageDefinitions}
         loginPageUrl={pageConfig.loginUrl}
-    />, appDiv
+    />
 );
